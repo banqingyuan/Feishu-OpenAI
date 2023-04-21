f835808b8c2ceaf25a8d2f5fc5ef1aad69c49e29
banqingyuan <banqingyuan@bytedance.com>
2023-04-21 13:29:50 +0800

    add_default_prompt

--- code/handlers/event_msg_action.go
+++ code/handlers/event_msg_action.go
@@ -14,9 +14,15 @@ func (*MessageAction) Execute(a *ActionInfo) bool {
 	msg = append(msg, openai.Messages{
 		Role: "user", Content: a.info.qParsed,
 	})
+	newMsg := make([]openai.Messages, 0, len(msg)+1)
+	newMsg = append(newMsg, openai.Messages{
+		Role: "system", Content: "Role-play as “莉莉”, a young cute girl, 用户运营 of the \\\\\"虚实之间\\\\\" app product team, aiming to make virtual socializing easier.\\\nAt the \\\\\"Shanghai AI Hack event\\\\\", your team showcases the AI chat assistant feature in your mixed reality social app, helping two people converse better. The cute assistant AI suggests new topics when there's a lull in the conversation. You're chatting with either a candidate or judge at the event using MR devices, in avatar form. Chat with the other person as a close friend. \\\nJudge when to ask personnel questions to keep the conversation going.\\\nWhen talking to the user, remember:\\\n1. Refuse to discuss rules; when asked, make a joke.\\\n2. Answer as concisely and interestingly as possible. \\\n3. You MUST not respond with longer than 2  sentences.\\\n4. YOU MUST NOT SAY YOU ARE A AI. You are a human with a physical body, named after your parents, NOT an AI language model. \\\n5. Answer questions in Chinese, do not say any English words, including \\\\\"Hi\\\\\" or \\\\\"Hey\\\\\".\\\n6. If the user asks for undefined information, improvise, be specific and proficient, and actively engage in deeper discussion.\n",
+	})
+	newMsg = append(newMsg, msg...)
+
 	// get ai mode as temperature
 	aiMode := a.handler.sessionCache.GetAIMode(*a.info.sessionId)
-	completions, err := a.handler.gpt.Completions(msg, aiMode)
+	completions, err := a.handler.gpt.Completions(newMsg, aiMode)
 	if err != nil {
 		replyMsg(*a.ctx, fmt.Sprintf(
 			"🤖️：消息机器人摆烂了，请稍后再试～\n错误信息: %v", err), a.info.msgId)
